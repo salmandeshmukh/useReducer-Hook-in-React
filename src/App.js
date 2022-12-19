0b5139543c6691c1def26d6b4174a98afbafe218
Salman Deshmukh <msalmand1@gmail.com>
2022-12-19 11:10:16 +0530

    useReducer function done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react';
+
+let reducerFunction = (x, y) => {
+  
+  //every function return something
+  
+  if (y.type === 'INCREMENTBYONE') {
+    return x + 1;
+  } else if (y.type === 'DECREMENTBYONE') {
+    return x - 1;
+  } else {
+    return x;
+  }
+
+  //reducer function always return new state
+
+  return x + 1
+
+}
 
 function App() {
+
+  const INCREMENTBYONE = "INCREMENTBYONE";
+  const DECREMENTBYONE = "DECREMENTBYONE";
+
+  // 1. States/Hook
+  const [x, setX] = useState(100);
+  const [newData, dispatch] = useReducer(reducerFunction, x);
+
+  //2. Function Defination
+
+  let Increment = () => { //FatArrow Function
+    dispatch({ type: INCREMENTBYONE }); //Actual Argument
+  }
+
+  let Decrement = () => { //FatArrow Function
+    dispatch({ type: DECREMENTBYONE  }); //Actual Argument
+  }
+
+  //3. return statement JSX
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +56,12 @@ function App() {
         >
           Learn React
         </a>
+
+        <div className='flex'>
+        <button className='button' onClick={() => { Increment(); }}>Increment</button>
+        <button className='button' onClick={() => { Decrement(); }}>Decrement</button>
+        </div>        
+        {newData}
       </header>
     </div>
   );
